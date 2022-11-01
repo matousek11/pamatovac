dc15cc88c868549b7580cdc38b3c2aa6fe989aca
lukas matousek <matousek1199@seznam.cz>
2022-11-01 20:53:04 +0100

    Přidejte soubory projektu.

---
+++ pamatovac/pamatovac/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pamatovac
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void Continue_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new WritePage());
+        }
+    }
+}
---
+++ pamatovac/pamatovac/WritePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pamatovac
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WritePage : ContentPage
+    {
+        public WritePage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void Save_Button_Clicked(object sender, EventArgs e)
+        {
+            string name = nameEntry.Text;
+
+        }
+
+        private void Name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = e.NewTextValue;
+        }
+    }
+}
